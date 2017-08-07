0a8157b536532a36477b76c805f422d6676b2c33
yunge5 <1510938176@qq.com>
2017-08-07 16:19:16 +0800

    修改test

--- src/demo/test/test.vue
+++ src/demo/test/test.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="test">
-		<h1>按钮类</h1>
+		<h5>按钮类</h5>
 		<t-button type="primary" class="actives" :disabled="true">disabled</t-button>
 	    <t-button type="primary">主按钮</t-button>
 	    <br><br>
@@ -38,44 +38,18 @@
 	      </t-button>
 	    </t-button-group>
 	    <br><br>
-	     <t-dropdown trigger="click">
-		     <t-button type="outline-primary">
+	    <h5>下拉菜单</h5>
+	    <t-dropdown trigger="click">
+		    <t-button type="outline-primary">
 		        下拉菜单
 		        <t-icon type="chevron-down"></t-icon>
-		      </t-button>
+		    </t-button>
 		    <t-dropdown-menu slot="list">
 		      <t-dropdown-item>选项一</t-dropdown-item>
 		      <t-dropdown-item>选项二</t-dropdown-item>
 		      <t-dropdown-item>选项三</t-dropdown-item>
 		    </t-dropdown-menu>
-		  </t-dropdown>
-	    <br><br>
-		<t-breadcrumb>
-	        <t-breadcrumb-item href="/">Home</t-breadcrumb-item>
-	        <t-breadcrumb-item href="/components">Components</t-breadcrumb-item>
-	        <t-breadcrumb-item>Breadcrumb</t-breadcrumb-item>
-	    </t-breadcrumb>
-	    <br><br>
-	    <t-breadcrumb>
-		    <t-breadcrumb-item href="/">
-		      <t-icon type="home-outline"></t-icon> Home
-		    </t-breadcrumb-item>
-		    <t-breadcrumb-item href="/components/breadcrumb">
-		      <t-icon type="buffer"></t-icon> Components
-		    </t-breadcrumb-item>
-		    <t-breadcrumb-item>
-		      <t-icon type="pound"></t-icon> Breadcrumb
-		    </t-breadcrumb-item>
-		  </t-breadcrumb>
-		<br><br>
-		<t-back-top></t-back-top>
-		<t-back-top :height="100" :bottom="200" :duration="300">
-        	<span class="top">
-        		<t-icon type="account-box-outline" size="36"></t-icon>
-        	</span>
-    	</t-back-top>
-    	<br><br>
-
+		</t-dropdown>
     	<t-dropdown trigger="click" style="margin-left: 20px">
 		    <a href="javascript:void(0)">
 		      更多操作
@@ -98,11 +72,30 @@
 		      <t-dropdown-item>选择四</t-dropdown-item>
 		    </t-dropdown-menu>
 		</t-dropdown>
-
+	    <br><br>
+	    <h5>导航</h5>
+		<t-breadcrumb>
+	        <t-breadcrumb-item href="/">Home</t-breadcrumb-item>
+	        <t-breadcrumb-item href="/components">Components</t-breadcrumb-item>
+	        <t-breadcrumb-item>Breadcrumb</t-breadcrumb-item>
+	    </t-breadcrumb>
+	    <t-breadcrumb>
+		    <t-breadcrumb-item href="/">
+		      <t-icon type="home-outline"></t-icon> Home
+		    </t-breadcrumb-item>
+		    <t-breadcrumb-item href="/components/breadcrumb">
+		      <t-icon type="buffer"></t-icon> Components
+		    </t-breadcrumb-item>
+		    <t-breadcrumb-item>
+		      <t-icon type="pound"></t-icon> Breadcrumb
+		    </t-breadcrumb-item>
+		  </t-breadcrumb>
 		<br><br>
+		<h5>分页</h5>
 		<t-pager :total="100"></t-pager>
 		<t-pager :total="100" :show-elevator="true"></t-pager>
 		<br><br>
+		<h5>步骤条</h5>
 		<t-steps :current="1" size="sm">
 	      <t-step title="提交完成" desc="This is a description"></t-step>
 	      <t-step title="正在处理" desc="This is a description"></t-step>
@@ -113,57 +106,74 @@
 	      <t-step title="正在处理" desc="This is a description" icon="window-close"></t-step>
 	      <t-step title="审核通过" desc="This is a description"></t-step>
 	    </t-steps>
+	    <t-steps :current="1" direction="vertical">
+			<t-step title="提交完成"></t-step>
+			<t-step title="正在处理"></t-step>
+			<t-step title="审核通过"></t-step>
+	    </t-steps>
 	    <br><br>
+	    <h5>tabs选项卡</h5>
 	    <t-tabs>
 	        <t-tab-panel label="标签一" name="tab-1" icon="window-close">标签一内容</t-tab-panel>
 	        <t-tab-panel label="标签二" name="tab-2">标签二内容</t-tab-panel>
 	        <t-tab-panel label="标签三" name="tab-3">标签三内容</t-tab-panel>
-	    </t-tabs>
+	    </t-tabs>	  
 	    <br><br>
-	    <t-checkbox >Checkbox</t-checkbox>
-	    <t-checkbox disabled>Checkbox</t-checkbox>
-	    <br><br>
+	    <h5>日期选择</h5>
 	    <div style="margin-bottom: 15px">
-	    	<h6>日期选择</h6>
 	    	<t-date-picker v-model="singleDateVal" v-on:date-change="onChange" placeholder="please select date"></t-date-picker>
-	  </div>
-	  <br><br>
-	  <t-form :model="formTop" :rules="ruleFormLabel" label-position="top">
-	      <t-form-item label="活动名称" prop="input1">
-	        <t-input v-model="formTop.input1" placeholder="表单尺寸大 && 标题顶对齐"></t-input>
-	      </t-form-item>
-	      <t-form-item label="选择器" >
-	        <t-select v-model="formTop.select" placeholder="请选择" class="w-100">
-	          <t-option value="beijing">北京市</t-option>
-	          <t-option value="shanghai">上海市</t-option>
-	          <t-option value="shenzhen">深圳市</t-option>
-	        </t-select>
-	      </t-form-item>
-	      <t-form-item label="是否订阅" prop="subscription">
-	        <t-switch>
-		      <span slot="open">开</span>
-		      <span slot="close">关</span>
-		    </t-switch>
-	      </t-form-item>
-	      <t-form-item label="选择器" >
-			<t-radio-group v-model="formTop.radio">
-		      <t-radio label="radio1"></t-radio>
-		      <t-radio label="radio2"></t-radio>
-		    </t-radio-group>
-	      </t-form-item>
-	    </t-form>
+	    </div>
 	    <br><br>
+	    <h5>表单相关</h5>
+	    <t-form :model="formTop" :rules="ruleFormLabel" label-position="top">
+	        <t-form-item label="活动名称" prop="input1">
+	            <t-input v-model="formTop.input1" placeholder="表单尺寸大 && 标题顶对齐"></t-input>
+	        </t-form-item>
+	        <t-form-item label="选择器" >
+	            <t-select v-model="formTop.select" placeholder="请选择" class="w-100">
+	                <t-option value="beijing">北京市</t-option>
+	                <t-option value="shanghai">上海市</t-option>
+	            	<t-option value="shenzhen">深圳市</t-option>
+	            </t-select>
+	        </t-form-item>
+	        <t-form-item label="是否订阅" prop="subscription">
+		        <t-switch>
+			      <span slot="open">开</span>
+			      <span slot="close">关</span>
+			    </t-switch>
+	      	</t-form-item>
+	        <t-form-item label="Radio" >
+				<t-radio-group v-model="formTop.radio">
+			      <t-radio label="radio1"></t-radio>
+			      <t-radio label="radio2"></t-radio>
+			    </t-radio-group>
+	        </t-form-item>
+	        <t-form-item label="Checkbox" >
+		        <t-checkbox >Checkbox</t-checkbox>
+		        <t-checkbox disabled>Checkbox</t-checkbox>
+	        </t-form-item>
+	    </t-form>
+	    <h5>滑块</h5>	    
 	    <t-slider v-model="value1"></t-slider>
 	    <t-slider v-model="value2" range></t-slider>
 	    <t-slider v-model="value3" range disabled></t-slider>
 	    <t-slider v-model="value8" :step="10" show-stops data-unit="Number"></t-slider>
 	    <br><br>
+	    <h5>标签</h5>
 	    <t-badge count="10" state='success' class="badge-circle">
 	    </t-badge>
 	    <br><br>
+	    <h5>表格</h5>
 	    <t-table border :columns="customColumns" :data="data">
   	</t-table>
 	<br><br>
+	<t-back-top></t-back-top>
+		<t-back-top :height="100" :bottom="200" :duration="300">
+        	<span class="top">
+        		<t-icon type="account-box-outline" size="36"></t-icon>
+        	</span>
+    	</t-back-top>
+		<br><br>
 	</div>
 </template>
 <script>
@@ -261,9 +271,10 @@ export default {
 }
 </script>
 <style scoped lang="scss">
+	h5{margin-bottom: 10px;}
 	.test{
 		height: 2500px;;
-		margin-top: 100px;
+		margin-top: 50px;
 		padding: 0 50px;
 		background-color:transparent;
 	}
