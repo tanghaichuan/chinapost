f3ea20d325a3f041a112b86f16e1981af8e014ab
tanghaichuan <764505165@qq.com>
2017-08-10 17:31:03 +0800

    增加系统菜单模块

--- src/module/index/header.vue
+++ src/module/index/header.vue
@@ -7,13 +7,16 @@
         <span class="text-xxl align-middle ml-1">Veris</span>
       </a>
     </t-tooltip>
-
+    
     <div class="mymenu">
-      <t-tooltip content="系统菜单" placement="bottom">
-        <a href="javascript:;" class="menu-icon">
-          <t-icon type="apps" size="26"></t-icon>
-        </a>
-      </t-tooltip>
+      <div class="mymenu-block--muenu" ref="systemMenu">
+        <t-tooltip content="系统菜单" placement="bottom" @click.native="showSystemMenu=!showSystemMenu">
+          <a href="javascript:;" class="menu-icon">
+            <t-icon type="apps" size="26"></t-icon>
+          </a>
+        </t-tooltip>
+        <system-menu v-show="showSystemMenu" :menuList="menuList"></system-menu>
+      </div>
       <t-tooltip content="历史记录" placement="bottom">
         <a href="javascript:;" :class="['menu-icon ml-2',{'active':showHistoryMenu}]" @click="historyMenuHandle">
           <t-icon type="backup-restore" size="26"></t-icon>
@@ -38,9 +41,9 @@
         </t-dropdown-menu>
       </t-dropdown>
     </div>
-    <t-button :class="['pl-3','pr-3']" style="{height:50px;}" @click.stop.prevent="showCustom"><i class="aid aid-menu"></i></t-button>
+    <t-button slot="appendbtn" :class="['pl-3','pr-3']" style="{height:50px;}"><i class="aid aid-menu"></i></t-button>
     <t-tooltip content="便签 & 待办事项" placement="bottom" class=" ml-auto">
-      <a href="javascript:;" class="menu-icon">
+      <a href="javascript:;" class="menu-icon" @click.stop.prevent="showCustom">
         <t-icon type="calendar-plus" size="26"></t-icon>
       </a>
     </t-tooltip>
@@ -87,11 +90,305 @@ import {
   mapGetters
 } from 'vuex'
 import historyMenu from './historyMenu.vue'
+import systemMenu from './systemMenu.vue'
 import invokers from '../../invokers'
 export default {
   data() { // store data
     return {
-      historyData: []
+      historyData: [],
+      showSystemMenu: false,
+      menuList:[
+            {
+              name: '营销管理',
+              children:[
+                {
+                  name: '统计报表',
+                  url: '/trouble/1'
+                },
+                {
+                  name: '活动管理',
+                  children:[
+                    {
+                      name: '活动视图',
+                      url: '/trouble/2'
+                    },
+                    {
+                      name: '问卷统计',
+                      url: '/trouble/3'
+                    }
+                  ]
+                },
+                {
+                  name: '细分管理',
+                  children:[
+                    {
+                      name: '共享列表',
+                      url: '/trouble/4'
+                    },
+                    {
+                      name: '市场细分',
+                      url: '/trouble/5'
+                    }
+                  ]
+                },
+                {
+                  name: '营销配置',
+                  children:[
+                    {
+                      name: '频次控制',
+                      url: '/trouble/6'
+                    },
+                    {
+                      name: '活动类型',
+                      url: '/trouble/7'
+                    }
+                  ]
+                }
+              ]
+            },
+            {
+              name: '故障单',
+              children:[
+                {
+                  name: '工单工作区',
+                  url: '/trouble/8'
+                },
+                {
+                  name: '工单统计',
+                  url: '/trouble/9'
+                },
+                {
+                  name: '工单配置',
+                  children:[
+                    {
+                      name: '工单类型配置',
+                      url: '/trouble/10'
+                    },
+                    {
+                      name: '离岗规则配置',
+                      url: '/trouble/11'
+                    },
+                    {
+                      name: '自动建单配置',
+                      url: '/trouble/12'
+                    },
+                  ]
+                }
+              ]
+            },
+            {
+              name: '销售',
+              children:[
+                {
+                  name: '销售管理',
+                  children:[
+                    {
+                      name: '线索管理',
+                      url: '/trouble/13'
+                    },
+                    {
+                      name: '商机管理',
+                      url: '/trouble/14'
+                    },
+                    {
+                      name: '建议书管理',
+                      url: '/trouble/15'
+                    },
+                    {
+                      name: '客户竞争者管理',
+                      url: '/trouble/16'
+                    },
+                    {
+                      name: '销售漏斗',
+                      url: '/trouble/17'
+                    },
+                    {
+                      name: '商机追踪',
+                      url: '/trouble/18'
+                    },
+                    {
+                      name: '未分配全局线索池',
+                      url: '/trouble/19'
+                    }
+                  ]
+                },
+                {
+                  name: '销售助手',
+                  children:[
+                    {
+                      name: '销售任务',
+                      url: '/trouble/20'
+                    },
+                    {
+                      name: '销售记录',
+                      url: '/trouble/21'
+                    },
+                    {
+                      name: '销售账户',
+                      url: '/trouble/22'
+                    },
+                    {
+                      name: '销售日程安排',
+                      url: '/trouble/23'
+                    },
+                    {
+                      name: '统一分配日志',
+                      url: '/trouble/24'
+                    },
+                  ]
+                },
+                {
+                  name: '销售层级管理',
+                  children:[
+                    {
+                      name: '销售组织管理',
+                      url: '/trouble/25'
+                    },
+                    {
+                      name: '操作员管理',
+                      url: '/trouble/26'
+                    }
+                  ]
+                },
+                {
+                  name: '销售分析',
+                  children:[
+                    {
+                      name: '销售洞察',
+                      url: '/trouble/27'
+                    },
+                    {
+                      name: '销售仪表盘',
+                      url: '/trouble/28'
+                    }
+                  ]
+                }
+              ]
+            },
+            {
+              name: '合同',
+              children:[
+                {
+                  name: '合同管理',
+                  children:[
+                    {
+                      name: '合同编辑',
+                      url: '/trouble/29'
+                    }
+                  ]
+                },
+                {
+                  name: '模版管理',
+                  children:[
+                    {
+                      name: '协议条款模版',
+                      url: '/trouble/30'
+                    },
+                    {
+                      name: '协议模版',
+                      url: '/trouble/31'
+                    }
+                  ]
+                },
+                {
+                  name: '合同设置',
+                  children:[
+                    {
+                      name: '协议类型管理',
+                      url: '/trouble/32'
+                    },
+                    {
+                      name: '标签管理',
+                      url: '/trouble/33'
+                    },
+                    {
+                      name: '协议层级管理',
+                      url: '/trouble/34'
+                    }
+                  ]
+                },
+                {
+                  name: '合同跟踪',
+                  children:[
+                    {
+                      name: '监控规则配置',
+                      url: '/trouble/35'
+                    },
+                    {
+                      name: '监控结束',
+                      url: '/trouble/36'
+                    }
+                  ]
+                }
+              ]
+            },
+            {
+              name: '知识库',
+              children:[
+                {
+                  name: 'Knowledge Homepage'
+                },
+                {
+                  name: 'Application for Approval'
+                },
+                {
+                  name: 'Article Management'
+                },
+                {
+                  name: 'My Article'
+                },
+                {
+                  name: 'Template Management'
+                },
+                {
+                  name: 'Business Type Management'
+                },
+                {
+                  name: 'Recommended Article Management'
+                },
+                {
+                  name: 'Recycle Bin'
+                },
+                {
+                  name: 'Public Favorites Management'
+                },
+                {
+                  name: 'Personal Favorites Management'
+                },
+                {
+                  name: 'Operation Log'
+                },
+                {
+                  name: 'Search Hot Article'
+                },
+                {
+                  name: 'Article Hits Statistics'
+                },
+                {
+                  name: 'Article Hits Details'
+                },
+                {
+                  name: 'Lexicon Management'
+                },
+                {
+                  name: 'Ticket Management'
+                }
+              ]
+            },
+            {
+              name: '系统配置',
+              children:[
+                {
+                  name: 'Bulletin Management',
+                  url: '/trouble/37'
+                },
+                {
+                  name: 'Bulletin Catalog Management',
+                  url: '/trouble/38'
+                }
+              ]
+            }
+          ],
     }
   },
   props: { // props data
@@ -103,7 +400,6 @@ export default {
   },
   methods: { // methods
     showCustom() {
-      // 点击菜单
       console.log(1);
     },
     historyMenuHandle() {
@@ -115,7 +411,10 @@ export default {
     },
     ...mapMutations({
       changeMenu: 'CHANGE_MENU'
-    })
+    }),
+    closeSystemMenu() {
+      this.showSystemMenu = false;
+    }
   },
   created() {
     this.$domains.cnpost.get(this.$services.GET_HISTORY).then((res) => {
@@ -124,10 +423,16 @@ export default {
   },
   mounted() {
     //do something after mounting vue instance
-
+    let stopEl = this.$refs.systemMenu;
+    document.addEventListener('click', this.closeSystemMenu);
+    stopEl.addEventListener('click', e => e.stopPropagation());
+  },
+  destroyed() {
+    document.removeEventListener('click', this.closeSystemMenu);
   },
   components: {
-    historyMenu
+    historyMenu,
+    systemMenu
   }
 }
 </script>
@@ -135,12 +440,14 @@ export default {
 .header {
     border-bottom: 1px solid #d9d9d9;
     box-shadow: 0 0 8px rgba(0, 0, 0,.14);
-    position: relative;
-    z-index: 3;
     .mymenu {
         height: 50px;
         // padding: 10px 0;
         line-height: 50px;
+        .mymenu-block--muenu{
+          display: inline-block;
+          position: relative;
+        }
     }
     .menu-icon {
         background: #f5f5f5;
