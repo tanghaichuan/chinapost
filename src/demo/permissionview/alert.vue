1e960a01ea84332bf33ae586b4c0c093932187b6
vicson <317516763@qq.com>
2017-08-18 17:52:36 +0800

    修改弹窗组件问题

--- src/demo/permissionview/alert.vue
+++ src/demo/permissionview/alert.vue
@@ -5,7 +5,7 @@
     <div class="tags mb-3">
       <span class="text-sm mr-2">已选择:</span>
       <!-- @on-close="handleClose(item,index) -->
-      <t-tag v-for="(item,index) in tags" :key="item" type="text">{{ item.name }}</t-tag>
+      <t-tag v-for="(item,index) in tags" @on-close="deleteTags(item)" :key="item" type="text">{{ item.name }}</t-tag>
       <!-- <t-button type="primary" size="sm" @click="selectPerson">添加标签</t-button> -->
     </div>
     <div class="contentList">
@@ -20,7 +20,7 @@
           <h1 class="listTitle text-md p-1 mb-0">选择人员</h1>
         </div>
         <div class="nameList">
-          <t-checkbox v-model="single" @on-change="selectAll" class="pl-2">全选</t-checkbox>
+          <t-checkbox v-model="allSel" @on-change="selectAll" class="pl-2">全选</t-checkbox>
           <ul class="list">
             <li class="list-item" v-for="(item,index) in personList" @click="selectPerson(item)">
               <span class="name" ref="nameVal">{{item.name}}</span>
@@ -39,6 +39,7 @@ export default {
     return {
       modal: false,
       tags: [],
+      selItems: 0,
       single: false,
       personList: [{
           name: '张三',
@@ -110,47 +111,90 @@ export default {
     },
     selectAll(selsectAll) {
       // 点击全选
-
       if (selsectAll) {
+        this.tags = []
+        this.selItems = this.personList.length
         for (let i = 0; i < this.personList.length; i++) {
-          console.log(i);
-          // this.personList[i].isSelect = selsectAll
-          // this.tags.push(this.personList[i])
+
+          this.personList[i].isSelect = selsectAll
+          this.tags.push(this.personList[i])
+          console.log(this.tags);
         }
       } else {
+        this.selItems = 0
         for (let i = 0; i < this.personList.length; i++) {
-          // this.personList[i].isSelect = selsectAll
-          // this.tags = []
+          this.personList[i].isSelect = selsectAll
+          this.tags = []
+          console.log(this.tags);
         }
       }
     },
-    selectPerson(item) {
-      if (item.isSelect) {
-        item.isSelect = !item.isSelect
-        for (var j = 0; j < this.tags.length; j++) {
-          console.log(j);
-          // if(this.tags[i].id == item.id){
-          //   this.tags.splice(i,1)
-          // }
+    selectPerson({
+      id,
+      name,
+      isSelect
+    }) {
+      // 如果当前项是选中状态
+      if (isSelect) {
+        this.selItems--
+          //把 list 里面设置为 false
+          for (let i = 0; i < this.personList.length; i++) {
+            if (this.personList[i].id == id) {
+              this.personList[i].isSelect = !isSelect
+            }
+          }
+        for (let j = 0; j < this.tags.length; j++) {
+          if (this.tags[j].id == id) {
+            this.tags.splice(j, 1)
+          }
         }
       } else {
-        this.tags.push(item)
+        this.selItems++
+          // 如果不是选中状态就添加选中状态
+          for (let i = 0; i < this.personList.length; i++) {
+            if (this.personList[i].id == id) {
+              this.personList[i].isSelect = !isSelect
+            }
+          }
+
+        this.tags.push({
+          id,
+          name,
+          isSelect
+        })
       }
+
+    },
+    deleteTags({
+      id,
+      name,
+      isSelect
+    }) {
+      // 删除 tags
+      this.selItems--;
+
+      for (let i = 0; i < this.personList.length; i++) {
+        if (this.personList[i].id == id) {
+          this.personList[i].isSelect = false
+        }
+      }
+      for (let j = 0; j < this.tags.length; j++) {
+        if (this.tags[j].id == id) {
+          this.tags.splice(j, 1)
+        }
+      }
+
+    }
+  },
+  computed: {
+    allSel() {
+      let personLength = this.personList.length
+      return this.selItems >= personLength ? true : false
+      console.log(1);
     }
   },
   watch: {
-    //  single() {
-    //    for(let i = 0;i<this.showIcon.length;i++){
-    //      this.showIcon[i] = !this.showIcon[i];
-    //    }
-    //    if(this.showIcon[0] === true){
-    //      for(let i =0;i<this.names.length;i++){
-    //        this.name[i] = this.names[i]
-    //      }
-    //    } else {
-    //      this.name = []
-    //    }
-    //  }
+
   },
   created() {}
 }
--- src/module/index/home.vue
+++ src/module/index/home.vue
@@ -25,6 +25,7 @@ export default {
 .home-container {
     background: #F4F8F9;
     height: 100%;
+    padding: 30px 0 70px;
     text-align: center;
     img {
         text-align: center;
