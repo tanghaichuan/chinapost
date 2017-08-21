90dead4525980f7ac9e51dd77ae06d069bd8268e
baotonghui <877361655@qq.com>
2017-08-21 15:31:16 +0800

    对齐 查询

---
+++ src/demo/owns/formlist/alignment.vue
@@ -0,0 +1,64 @@
+<style scoped lang="scss">
+    .alignment {
+        padding: 16px;
+    }
+
+    .bor-table {
+        height: 24px;
+    }
+
+    .name::before {
+        content: "*";
+        display: inline-block;
+        margin-left: 4px;
+
+        font-size: 12px;
+        color: #FF5F5F;
+    }
+
+</style>
+<template>
+    <t-form class=" alignment">
+        <table>
+            <tr>
+                <td><span class="name">生效方式：</span></td>
+                <td>
+                    <t-input v-model="value" placeholder="" style="width: 300px"></t-input>
+                </td>
+                <td>
+                    <t-date-picker style="width:250px" v-model="singleDateTimeVal" type="dateTime"
+                                   v-on:date-change="onChange" placeholder="输入时间"></t-date-picker>
+                </td>
+            </tr>
+            <tr class="bor-table">
+                <td colspan="3"></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td colspan="2">
+                    <t-button type="info">提交</t-button>
+                    <t-button class="ml-2" @click="cancel">取消</t-button>
+                </td>
+
+            </tr>
+        </table>
+
+    </t-form>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                value: '',
+                singleDateTimeVal: ''
+            }
+        },
+        methods: {
+            cancel() {
+                this.value = '';
+                this.singleDateTimeVal = ''
+            }
+
+        }
+    }
+</script>
---
+++ src/demo/owns/formlist/highquery.vue
@@ -0,0 +1,133 @@
+<style scoped lang="scss">
+    table {
+        border-collapse: collapse;
+        border-spacing: 0;
+    }
+
+    th, td {
+        padding: 0;
+    }
+
+    .alignment {
+        border: 1px dashed #DDDAD6;
+        padding: 1rem;
+        .name {
+            text-align: right;
+        }
+
+        .name::before {
+            content: "*";
+            display: inline-block;
+            margin-left: 4px;
+
+            font-size: 12px;
+            color: #FF5F5F;
+        }
+    }
+    .bor-table{
+        height: 16px;
+    }
+    .name{
+        margin-right: 8px;
+        margin-left: 16px;
+    }
+
+
+</style>
+<template>
+    <div class="alignment">
+        <t-form :model="formItem" class="form">
+            <table>
+                <tr class="tr">
+                    <td>
+                        <div class="name">名字较长的标签:</div>
+                    </td>
+                    <td>
+                        <t-input placeholder=""></t-input>
+                    </td>
+                    <td>
+                        <div class="name">名字较长的标签:</div>
+                    </td>
+
+                    <td>
+                        <t-input placeholder=""></t-input>
+                    </td>
+                    <td>
+                        <div class="name">名字较长的标签:</div>
+                    </td>
+
+                    <td>
+                        <t-input placeholder=""></t-input>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="6" class="bor-table"></td>
+                </tr>
+                <tr>
+                    <td>
+                        <div class="name">标签:</div>
+                    </td>
+
+                    <td>
+                        <t-input placeholder=""></t-input>
+                    </td>
+                    <td>
+                        <div class="name">标签:</div>
+                    </td>
+
+                    <td>
+                        <t-input placeholder=""></t-input>
+                    </td>
+                    <td>
+                        <div class="name">标签:</div>
+                    </td>
+
+                    <td>
+                        <t-input placeholder=""></t-input>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="6" class="bor-table"></td>
+                </tr>
+                <tr>
+                    <td>
+                        <div class="name">标签:</div>
+                    </td>
+                    <td colspan="3">
+                        <t-input placeholder=""></t-input>
+                    </td>
+
+                </tr>
+                <tr>
+                    <td colspan="6" class="bor-table"></td>
+                </tr>
+                <tr>
+                    <td colspan="4">
+
+                    </td>
+                    <td colspan="2" class="clearfix">
+                        <div class="float-right">
+                            <t-button type="info" class="">搜索</t-button>
+                            <t-button class="ml-2">清空</t-button>
+                            <a href="javascript:void(0);" class="ml-2 text-info">简易搜索</a>
+                        </div>
+
+                    </td>
+                </tr>
+            </table>
+
+
+        </t-form>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                formItem: {
+                    input: ''
+                }
+            }
+        }
+    }
+</script>
