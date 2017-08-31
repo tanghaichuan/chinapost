42dd125306462129ce4432b2c835af9e1cc1a065
tanghaichuan <764505165@qq.com>
2017-08-31 09:20:32 +0800

    增加动态表单demo

---
+++ src/demo/test/dynamicForm.vue
@@ -0,0 +1,103 @@
+<template>
+    <div class="dynamic-form">
+        <t-form :model="userList" :rules="listRule" label-position="left" :label-span="3">
+            <item-form v-for="(item, index) in userList" :key="index" :userInfo="item"></item-form>
+        </t-form>
+    </div>
+</template>
+<script>
+
+
+import itemForm from './itemForm'
+export default {
+    name: 'dynamicForm',
+    data() {
+        return {
+            userList: [],
+            listRule: {
+                NAME: [
+                    { required: true, message: '姓名不能为空', trigger: 'blur' }
+                ]
+            },
+            userInfo: {
+                "CHA": [
+                    {
+                        "CODE": "NAME",
+                        "VALUE": "1",
+                        "DISP": '客户编码',
+                        "OPER_MODE": "010"
+                    },
+                    {
+                        "CODE": "NAME",
+                        "VALUE": "爱因斯坦",
+                        "DISP": '客户名称',
+                        "OPER_MODE": "010"
+                    },
+                    {
+                        "CODE": "SEX",
+                        "VALUE": "M",
+                        "DISP": "性别",
+                        "OPER_MODE": "02",
+                        "ENUM": [
+                            {
+                                "key": "M",
+                                "value": "男"
+                            },
+                            {
+                                "key": "F",
+                                "value": "女"
+                            },
+                            {
+                                "key": "U",
+                                "value": "其他"
+                            }
+                        ]
+                    },
+                    // {
+                    //     "CODE": "BIRTH_DATE",
+                    //     "VALUE": "1975-09-20",
+                    //     "DISP": 3,
+                    //     "OPER_MODE": "014"
+                    // },
+                    // {
+                    //     "CODE": "IS_CONTRACT_USER",
+                    //     "VALUE": "T",
+                    //     "DISP": 1,
+                    //     "OPER_MODE": "016"
+                    // },
+                    {
+                        "CODE": "REMARKS",//唯一 id
+                        "VALUE": "需要特殊照顾。\n非常重要！",// 值
+                        "DISP": '客户编码',
+                        "OPER_MODE": "013" //控件类型
+                    },
+                    // {
+                    //     "CODE": "联系地址",
+                    //     "VALUE": "北京中关村南路125号",
+                    //     "DISP": 2,
+                    //     "OPER_MODE": "015"
+                    // }
+                ]
+            },
+            form: [
+
+            ]
+        }
+    },
+    components: {
+        itemForm
+    },
+    computed: {
+
+    },
+    created() {
+        Object.keys(this.userInfo).forEach(item => {
+            this.userList.push(this.userInfo[item]);
+        })
+        console.log(this.userList);
+    }
+}
+</script>
+<style scoped lang="less">
+
+</style>
---
+++ src/demo/test/itemForm.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="item-form">
+        <render-form v-for="(item, index) in userInfo" :key="index" :user="item"></render-form>
+    </div>
+</template>
+<script>
+import renderForm from './renderForm'
+export default {
+    name: 'itemForm',
+    data() {
+        return {
+
+        }
+    },
+    props: {
+        userInfo: Array
+    },
+    components: {
+        renderForm
+    },
+    created() {
+
+    }
+}
+</script>
+<style scoped lang="less">
+.item-form{
+    zoom: 1;
+    &:after{
+        display: table;
+        overflow: hidden;
+        content:'';
+        clear: both;
+    }
+}
+</style>
