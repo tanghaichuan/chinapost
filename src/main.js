b9e89b19467ec147fa3cbe7ef94b5a892bf1f60a
tanghaichuan <764505165@qq.com>
2017-09-14 10:40:27 +0800

    引入lodash依赖库

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
-import { sync } from 'vuex-router-sync'
+import {sync} from 'vuex-router-sync'
 import taurus from 'aid-taurus-desktop'
+import * as _ from 'lodash/core' // lodash依赖库 _.func 调用
 // import VueI18n from 'vue-i18n'
 /* locale for aid-taurus-desktop */
-// import aidzhLocale from 'aid-taurus-desktop/src/locale/lang/zh-CN'
-// import aidenLocale from 'aid-taurus-desktop/src/locale/lang/en-US'
+// import aidzhLocale from 'aid-taurus-desktop/src/locale/lang/zh-CN' import
+// aidenLocale from 'aid-taurus-desktop/src/locale/lang/en-US'
 
 import store from './store'
 //import container from './container.vue'
@@ -12,44 +13,23 @@ import main from './app.vue'
 import bus from './bus'
 import router from './router'
 import './invokers'
-
 /* locale for project */
-// import zhLocale from './local/zh-CN'
-// import enLocale from './local/en-US'
-
-// Vue.use(VueI18n)
-
-// 生成本地化资源对象
-// const messages = {
-//   'en-US': {
-//     ...enLocale,
-//     ...aidenLocale
-//   },
-//   'zh-CN': {
-//     ...zhLocale,
-//     ...aidzhLocale
-//   }
-// }
-
-// const i18n = new VueI18n({
-//   locale: bus.lang, // 设置当前语言
-//   messages // 设置本地化资源
-// })
+// import zhLocale from './local/zh-CN' import enLocale from './local/en-US'
+// Vue.use(VueI18n) 生成本地化资源对象 const messages = {   'en-US': {     ...enLocale,
+// ...aidenLocale   },   'zh-CN': {     ...zhLocale,     ...aidzhLocale   } }
+// const i18n = new VueI18n({   locale: bus.lang, // 设置当前语言   messages //
+// 设置本地化资源 })
 
 Vue.use(taurus, {
-  // i18n: (path, options) => {
-  //   let value = i18n.t(path, options)
-  //   if (value !== null && value !== undefined) return value
-  //   return ''
-  // }
+  // i18n: (path, options) => {   let value = i18n.t(path, options)   if (value
+  // !== null && value !== undefined) return value   return '' }
 })
 
 // 将路由状态同步到store中
 sync(store, router)
 
 const app = new Vue({
-  store,
-  router,
+  store, router,
   // i18n,
   render(h) {
     return h(main)
@@ -57,7 +37,9 @@ const app = new Vue({
 })
 
 if (module.hot) {
-  module.hot.accept()
+  module
+    .hot
+    .accept()
 }
 
 app.$mount('#app')
