33417e4f9b262a54edf5c625fb8a8fb3d7d29e21
zhaoqingling <2998030587@qq.com>
2017-09-18 16:55:12 +0800

    删除多余文件

--- src/routes.js
+++ src/routes.js
@@ -10,16 +10,6 @@ const test = proxyLoad('demo/test/test', 'test')
 // const test = r => require.ensure([], () => r(require("demo/test/test")),
 // 'test')
 const tabel = r => require.ensure([], () => r(require('demo/test/table1')), 'test')
-const tabelDemo = r => require.ensure([], () => r(require('demo/table/table')), 'test')
-const comTable = r => require.ensure([], () => r(require('demo/owns/listTable/comperhensiveTable')), 'test')
-const editTable = r => require.ensure([], () => r(require('demo/owns/listTable/editTable')), 'test')
-const frozenTable = r => require.ensure([], () => r(require('demo/owns/listTable/frozenTable')), 'test')
-const permissionAlert = r => require.ensure([], () => r(require('demo/permissionview/alert')), 'test')
-const merge = r => require.ensure([], () => r(require('demo/owns/listTable/mergeTable')), 'test')
-const formStandard = r => require.ensure([], () => r(require('demo/owns/formlist/formStandard')), 'test')
-const highquery = r => require.ensure([], () => r(require('demo/owns/formlist/highquery')), 'test')
-const alignment = r => require.ensure([], () => r(require('demo/owns/formlist/alignment')), 'test')
-const popupEdit = r => require.ensure([], () => r(require('demo/owns/formlist/popupEdit')), 'test')
 const dynamicForm = r => require.ensure([], () => r(require('demo/test/dynamicForm')), 'test')
 const checkBoxTree = r => require.ensure([], () => r(require('demo/portal/components/checkBoxTree.vue')), 'test')
 
@@ -39,25 +29,13 @@ export default[
       }, {
         name: 'Permission',
         path: '/permissionview',
-        component: require('./demo/permissionview/view.vue')
+        component: require('module/clientmanage/wrapper/wrapper.vue')
       }
     ]
   }, {
     name : 'Login',
     path : '/login',
     component : require('./module/login/login.vue')
-  }, {
-    name : 'table示例',
-    path : '/demo-table',
-    component : tabelDemo
-  }, {
-    name : 'test',
-    path : '/test',
-    component : test
-  }, {
-    name : 'table',
-    path : '/table',
-    component : tabel
   }, {
     name : 'menu',
     path : '/menu',
@@ -68,22 +46,6 @@ export default[
         path: '',
         component: require('module/clientmanage/wrapper/wrapper.vue')
       }, {
-        name: 'comperhenTable',
-        path: 'comperhenTable',
-        component: comTable
-      }, {
-        name: 'frozeTable',
-        path: 'froze',
-        component: frozenTable
-      }, {
-        name: 'ediTable',
-        path: 'editTable',
-        component: editTable
-      }, {
-        name: 'mergeTable',
-        path: 'mergeTable',
-        component: merge
-      }, {
         name: '客户管理',
         path: 'clientInfo',
         component: require('module/clientmanage/customer.vue')
@@ -93,49 +55,8 @@ export default[
         component: require('module/clientmanage/company.vue')
       }
     ]
-  }, {
-    name : 'conperhensiveTable',
-    path : '/comTable',
-    component : comTable
-  }, {
-    name : 'permissionAlert',
-    path : '/alert',
-    component : permissionAlert
-  },
+  }, 
   // {   name : 'query',   path : '/query',   component : query },
-  {
-    name : 'editTable',
-    path : '/edit-table',
-    component : editTable
-  }, {
-    name : 'forzenTable',
-    path : '/frozen-table',
-    component : frozenTable
-  }, {
-    name : 'formStandard',
-    path : '/formStandard',
-    component : formStandard
-  }, {
-    name : 'highquery',
-    path : '/highquery',
-    component : highquery
-  }, {
-    name : 'alignment',
-    path : '/alignment',
-    component : alignment
-  }, {
-    name : 'popupEdit',
-    path : '/popupEdit',
-    component : popupEdit
-  }, {
-    name : 'dynamicForm',
-    path : '/dynamicForm',
-    component : dynamicForm
-  }, {
-    name : 'checkBoxTree',
-    path : '/checkBoxTree',
-    component : checkBoxTree
-  },
   ...client,
   ...cloud,
   ...osp, {
