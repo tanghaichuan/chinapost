28762ba97f31eb7600510145673e53fbd6386912
vicson <317516763@qq.com>
2017-09-25 09:57:20 +0800

    360首页

--- src/module/agentView/index.vue
+++ src/module/agentView/index.vue
@@ -15,7 +15,8 @@
         <t-tab-panel label="标签二" name="tab-2">标签二内容</t-tab-panel>
         <t-tab-panel label="标签三" name="tab-3">标签三内容</t-tab-panel>
         <t-tab-panel label="标签四" name="tab-4">标签四内容</t-tab-panel>
-        <t-tab-panel label="标签五" name="tab-5">标签五内容</t-tab-panel>
+        <t-tab-panel label="标签五" name="tab-5">标签五内容
+        </t-tab-panel>
       </t-tabs>
     </div>
     <div class="customer-share">
